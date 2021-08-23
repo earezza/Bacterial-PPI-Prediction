11546dbf8af552adf558e77452698a5d3b0142f8
erixazerro <39600627+erixazerro@users.noreply.github.com>
2021-08-23 13:38:37 -0400

    Add files via upload

---
+++ PREPROCESS/get_profiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+args=("$@")
+proteins=$(ls -A1 ${args[0]})
+mkdir blastpssm/
+mkdir blastout/
+for p in $proteins
+do
+  	psiblast -db "${args[1]}" -evalue 0.001 -query "${args[0]}""${p}" -out_ascii_pssm blastpssm/${p>
+        cat blastpssm/${p}.pssm | sed '1,3d' | tac | sed '1,6d' | tac | cut -d' ' -f8- | column -t | tr>
+        rm blastout/${p}.output
+        rm blastpssm/${p}.pssm
+done
+
+find "${args[0]}" -depth -name "*.txt.mtx" -exec sh -c 'f="{}"; mv -- "$f" "${f%.txt.mtx}"' \;
+rm -r blastpssm
+rm -r blastout
