1f4f0f07fda53d6320217013089139872115856d
erixazerro <39600627+erixazerro@users.noreply.github.com>
2021-09-02 12:35:27 -0400

    Update get_profiles.sh

--- PREPROCESS/get_profiles.sh
+++ PREPROCESS/get_profiles.sh
@@ -5,8 +5,8 @@ mkdir blastpssm/
 mkdir blastout/
 for p in $proteins
 do
-  	psiblast -db "${args[1]}" -evalue 0.001 -query "${args[0]}""${p}" -out_ascii_pssm blastpssm/${p>
-        cat blastpssm/${p}.pssm | sed '1,3d' | tac | sed '1,6d' | tac | cut -d' ' -f8- | column -t | tr>
+  	psiblast -db "${args[1]}" -evalue 0.001 -query "${args[0]}""${p}" -out_ascii_pssm blastpssm/${p}.pssm -out blastout/${p}.output -num_iterations 3
+        cat blastpssm/${p}.pssm | sed '1,3d' | tac | sed '1,6d' | tac | cut -d' ' -f8- | column -t | tr ' ' '\t' | tr -s '\t' > ${args[0]}"${p}.mtx"
         rm blastout/${p}.output
         rm blastpssm/${p}.pssm
 done
