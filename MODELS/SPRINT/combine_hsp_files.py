ec2722d2793cf5e11a9fd712627f7a4cdbcad4bb
erixazerro <39600627+erixazerro@users.noreply.github.com>
2021-11-05 20:14:41 -0400

    Add files via upload

--- MODELS/SPRINT/combine_hsp_files.py
+++ MODELS/SPRINT/combine_hsp_files.py
@@ -10,6 +10,7 @@ import os, argparse
 import numpy as np
 import pandas as pd
 import time
+import tqdm
 
 describe_help = 'python combine_hsp_files.py -f hash0.hsp hash1.hsp hash2.hsp hash3.hsp -r HSP/ -n hsps.hsp'
 parser = argparse.ArgumentParser(description=describe_help)
@@ -19,21 +20,17 @@ parser.add_argument('-n', '--name', help='Filename of resulting hsp file', type=
 args = parser.parse_args()
 
 # Read hsp file as pd.read_csv(file, sep='\n', header=None)
-def hsp_to_dict(hsp):
-    hsp_dict = {}
-    for i in range(0, hsp.shape[0]):
-        if hsp.iloc[i][0][0] == '>':
-            pairs = hsp.iloc[i][0]
-            hsp_dict[pairs] = list()
-        else:
-            hsp_dict[pairs].insert(len(hsp_dict[pairs]), hsp.iloc[i][0])
-            
-    return hsp_dict
-
-def hsp_dict_to_df(hsp):
-    df = pd.DataFrame(hsp.keys())
-    df.insert(1, 1, [ hsp[df.iloc[i][0]] for i in range(0, len(hsp)) ])
-    return df
+def convert_hsp_to_df(h):
+    pairs = h[h[0].str[0] == '>']
+    if pairs.index[-1] == h.index[-1] - 1:
+        hits = pairs.index.insert(len(pairs.index), h.index[-1] + 1)
+    else:
+        hits = pairs.index.insert(len(pairs.index), h.index[-1])
+    hsps = np.array([ h.iloc[hits[i]+1:hits[i+1]].values.flatten().tolist() for i in tqdm.tqdm(range(0, len(pairs.index))) ], dtype=object)
+    pairs.insert(1,1, hsps)
+    pairs.reset_index(drop=True, inplace=True)
+    return pairs
+    
 
 if __name__ == '__main__':
     
@@ -45,35 +42,33 @@ if __name__ == '__main__':
     # Read SPRINT hsp files
     hsps = []
     for f in args.files:
+        print('Reading file %s...'%f)
         hsps.append(pd.read_csv(f, sep='\n', header=None))
 
-    # Convert to dictionary for pairs: hsps
-    for h in range(0, len(hsps)):
-        hsps[h] = hsp_to_dict(hsps[h])
-    
     # Convert to DataFrames
     for h in range(0, len(hsps)):
-        hsps[h] = hsp_dict_to_df(hsps[h])
-    
+        print('Converting to dataframe %s...'%h)
+        hsps[h] = convert_hsp_to_df(hsps[h])
+
     # Combine all
-    hsp = pd.DataFrame()
-    for h in range(0, len(hsps)):
-        hsp = hsp.append(hsps[h], ignore_index=True)
+    print('Combining HSP dataframes...'%h)
+    hsp = pd.concat(hsps, ignore_index=True)
     
     # Group all hsps found for all pairs from all files
+    print('Grouping HSPs...')
     hsp = hsp.explode(hsp.columns[-1], ignore_index=True)
     group = hsp.groupby([hsp.columns[0]])[hsp.columns[1]].apply(lambda x: x.unique()).reset_index()
     
     # Write HSPs to file
-    hsps = pd.DataFrame(group[1].to_list(), index=group[1].index)
-    hsps.insert(0, 'Pair', group[0])
+    print('Writing to file %s...'%args.name)
     f = open(args.results + args.name, 'w')
-    for i in range(0, hsps.shape[0]):
-        for j in range(0, hsps.shape[1]):
-            if hsps.iloc[i][hsps.columns[j]] != None:
-                f.write(hsps.iloc[i][hsps.columns[j]] + '\n')
+    for i in range(0, group.shape[0]):
+        f.write(group.iloc[i][0] + '\n')
+        for j in group.iloc[i][1].tolist():
+            f.write(j + '\n')
     f.close()
     
     print("Time: %s"%round(time.time()-t_start, 4))
+    
     
     
