3b8d0228d9eb0673ee49e27d593b956ec47ef271
erixazerro <39600627+erixazerro@users.noreply.github.com>
2022-01-09 20:20:58 -0500

    Add files via upload

--- EVALUATION/evaluate_ppi.py
+++ EVALUATION/evaluate_ppi.py
@@ -4,6 +4,7 @@
 Description:
     Evaluates the performance of PPI predictions by plotting ROC and Precision-Recall curves.
     Plots prevalence-corrected curves for hypothetically imbalanced data.
+    Writes to file with evaluated metric results.
     
     Requires files to have no header and be whitespace-separated (.tsv).
     
@@ -26,6 +27,7 @@ Usage:
 """
 
 __all__ = ['recalculate_precision',
+           'recalculate_metrics_to_imbalance',
            'get_matching_pairs',
            ]
 
@@ -39,7 +41,6 @@ import numpy as np
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-
 describe_help = 'python evaluate_ppi.py -s SCORES/ -l labels.tsv -d 0.5 -r RESULTS/'
 parser = argparse.ArgumentParser(description=describe_help)
 parser.add_argument('-s', '--scores', help='Full path to scored PPIs (directory to cross-validation files or single test file path)', type=str)
@@ -47,7 +48,7 @@ parser.add_argument('-l', '--labels', help='Full path to labelled PPIs (.tsv fil
 parser.add_argument('-r', '--results', help='Path to directory for saving evaluation files and plots', 
                     type=str, default=os.getcwd()+'/EVALUATION/')
 parser.add_argument('-d', '--delta', help='Imbalance ratio as positives/total (e.g. balanced = 0.5) for estimate of performance on hypothetical imbalanced data', 
-                    type=float, nargs=1, required=False, default=0.5)
+                    type=float, default=0.5)
 parser.add_argument('-n', '--name', help='Name for saving files, default basename will be results directory name', 
                     type=str, default='')
 args = parser.parse_args()
@@ -57,24 +58,45 @@ if not os.path.exists(RESULTS_DIR):
     os.mkdir(RESULTS_DIR)
     
 if args.name == '':
-    args.name = RESULTS_DIR.split('/')[-2].lower()
+    args.name = RESULTS_DIR.split('/')[-2].lower().capitalize() + '_' + args.labels.split('/')[-1].split('.')[0]
 
 # Display ratio of positives:negatives
 RATIO = '1:' + str(int((1/args.delta) - 1))
 
 # Calculate estimate for prevalence-corrected precision on imbalanced data
-def recalculate_precision(df, precision, thresholds):
-    delta = 2*args.delta - 1
+def recalculate_precision(df, precision, thresholds, d):
+    delta = 2*d - 1
     new_precision = precision.copy()
     for t in range(0, len(thresholds)):
         tn, fp, fn, tp = metrics.confusion_matrix(df[1], (df[0] >= thresholds[t]).astype(int)).ravel()
-        tp = tp*(1 + delta)
-        fp = fp*(1 - delta)
-        new_precision[t] = tp/(tp+fp)
+        lpp = tp/(tp+fn)
+        lnn = tn/(tn+fp)
+        if d != 0.5:
+            new_precision[t] = (lpp*(1 + delta)) / ( (lpp*(1 + delta)) + ((1 - lnn)*(1 - delta)) )
+        else:
+            new_precision[t] = (lpp)/(lpp + (1-lnn))
     return new_precision
 
+# Recalculate metrics for imbalanced classification where d is num_positives/(num_positives + num_negatives)
+def recalculate_metrics_to_imbalance(tp, tn, fp, fn, d):
+    delta = 2*d - 1
+    # recall and specificity are unchanged
+    lpp = tp/(tp+fn)
+    lnn = tn/(tn+fp)
+    if d != 0.5:
+        accuracy = lpp*((1 + delta)/2) + lnn*((1 - delta)/2)
+        precision = (lpp*(1 + delta)) / ( (lpp*(1 + delta)) + ((1 - lnn)*(1 - delta)) )
+        f1 = (2*lpp*(1 + delta)) / ( ((1+lpp)*(1+delta)) + ((1-lnn)*(1-delta)) )
+        mcc = (lpp+lnn-1) / np.sqrt((lpp + (1-lnn)*( (1-delta)/(1+delta) ) )*( lnn + (1-lpp)*( (1+delta)/(1-delta) ) ))
+    else:
+        accuracy = (lpp + lnn)/2
+        precision = (lpp)/(lpp + (1-lnn))
+        f1 = (2*lpp)/(2 + lpp - lnn)
+        mcc = (lpp + lnn - 1) / np.sqrt( (lpp + (1-lnn))*(lnn + (1-lpp)) )
+        
+    return round(accuracy, 5), round(precision, 5), round(lpp, 5), round(lnn, 5), round(f1, 5), round(mcc, 5)
+
 # Get labels for PPIs
-# NOTE: For SPRINT, labels are already in column in df
 def get_matching_pairs(df_1, df_2):
     # Get matches using PPI ordering of smaller df
     if df_1.shape > df_2.shape:
@@ -105,7 +127,7 @@ if __name__ == '__main__':
     if os.path.isdir(args.scores):
         # For cross-validation tested PPI subsets
         files = os.listdir(path=args.scores)
-        files = [ x for x in files if 'fold' in x and 'pos' not in x and 'neg' not in x and 'train' not in x.lower() ]
+        files = [ x for x in files if 'prediction' in x and '.pos' not in x and '.neg' not in x ]
         files.sort()
     else:
         # For single file tested PPIs
@@ -124,34 +146,97 @@ if __name__ == '__main__':
     pr_aucs = {}
     recalls = {}
 
+    # Additional metrics
+    avg_accuracy = []
+    avg_precision = []
+    avg_recall = []
+    avg_specificity = []
+    avg_f1 = []
+    avg_mcc = []
+
     df_pred_avg = pd.DataFrame()
     fold = 0
     for k in files:
         
         # Isolate k-fold subset
-        print('===== Fold - %s ====='%fold)
+        print('\n===== Fold - %s ====='%fold)
         
-        # Read predictions for k-fold
-        df_pred = pd.read_csv(args.scores + k, delim_whitespace=True, header=None)
+        # Read predictions for k-fold set or single test set
+        if os.path.isdir(args.scores):
+            df_pred = pd.read_csv(args.scores + k, delim_whitespace=True, header=None)
+        else:
+            df_pred = pd.read_csv(args.scores, delim_whitespace=True, header=None)
         
-        # Get labels from cross-validation subsets
-        if '_SPRINT_' not in k and ('SPRINT' not in args.scores and 'CME' not in args.scores):
-            df_pred = get_matching_pairs(df_pred, df_labels)
-            df_pred.drop(columns=[0, 1], inplace=True)
-            df_pred.rename(columns={'2_x': 0, '2_y': 1}, inplace=True)
-
+        # Get matching PPI labels for predictions
+        #if '_SPRINT_' not in k and ('SPRINT' not in args.scores and 'CME' not in args.scores):
+        df_pred = get_matching_pairs(df_pred, df_labels)
+        df_pred.drop(columns=[0, 1], inplace=True)
+        df_pred.rename(columns={'2_x': 0, '2_y': 1}, inplace=True)
+        #df_pred[[0, 1]] = df_pred[[1, 0]]
+        
         df_pred_avg = df_pred_avg.append(df_pred)
         
+        # Get other metrics at 0.5 threshold if predictions are probabilities (0 to 1) i.e. not SPRINT predictions
+        if df_pred[0].min() >= 0 and df_pred[0].max() <= 1:
+            
+            tn, fp, fn, tp = metrics.confusion_matrix(df_pred[1], (df_pred[0] + 1e-12).round()).ravel()
+            print('TP = %0.0f \nFP = %0.0f \nTN = %0.0f \nFN = %0.0f'%(tp, fp, tn, fn))
+            
+            # For imbalanced classification metrics
+            if args.delta != 0.5:
+                accuracy, precision, recall, specificity, f1, mcc = recalculate_metrics_to_imbalance(tp, tn, fp, fn, args.delta)
+                if np.isnan(accuracy, where=False): avg_accuracy.append(accuracy)
+                if np.isnan(precision, where=False): avg_precision.append(precision)
+                if np.isnan(recall, where=False): avg_recall.append(recall)
+                if np.isnan(specificity, where=False): avg_specificity.append(specificity)
+                if np.isnan(f1, where=False): avg_f1.append(f1)
+                if np.isnan(mcc, where=False): avg_mcc.append(mcc)
+            else:
+                try:
+                    accuracy = round((tp+tn)/(tp+fp+tn+fn), 5)
+                    avg_accuracy.append(accuracy)
+                except ZeroDivisionError:
+                    accuracy = np.nan
+                try:
+                    precision = round(tp/(tp+fp), 5)
+                    avg_precision.append(precision)
+                except ZeroDivisionError:
+                    precision = np.nan
+                try:
+                    recall = round(tp/(tp+fn), 5)
+                    avg_recall.append(recall)
+                except ZeroDivisionError:
+                    recall = np.nan
+                try:
+                    specificity = round(tn/(tn+fp), 5)
+                    avg_specificity.append(specificity)
+                except ZeroDivisionError:
+                    specificity = np.nan
+                try:
+                    f1 = round((2*tp)/(2*tp+fp+fn), 5)
+                    avg_f1.append(f1)
+                except ZeroDivisionError:
+                    f1 = np.nan
+                try:
+                    mcc = round( ( ((tp*tn) - (fp*fn)) / np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)) ), 5)
+                    avg_mcc.append(mcc)
+                except ZeroDivisionError:
+                    mcc = np.nan
+            
+            print('Accuracy =', accuracy, '\nPrecision =', precision, '\nRecall =', recall, '\nSpecificity =', specificity, '\nF1 =', f1, '\nMCC =', mcc)
+            
         # Evaluate k-fold performance and adjust for hypothetical imbalance
         precision, recall, thresholds = metrics.precision_recall_curve(df_pred[1], df_pred[0])
-        precision = recalculate_precision(df_pred, precision, thresholds)
+        if args.delta != 0.5:
+            precision = recalculate_precision(df_pred, precision, thresholds, args.delta)
         fpr, tpr, __ = metrics.roc_curve(df_pred[1], df_pred[0])
         if args.delta == 0.5:
             pr_auc = metrics.average_precision_score(df_pred[1], df_pred[0])
         else:
             pr_auc = metrics.auc(recall, precision)
         roc_auc = metrics.roc_auc_score(df_pred[1], df_pred[0])
-        print('auc_roc=', roc_auc, '\nauc_pr=', pr_auc)
+        
+        print('AUC_ROC = %0.5f'%roc_auc, '\nAUC_PR = %0.5f'%pr_auc)
         
         # Add k-fold performance for overall average performance
         tprs[fold] = tpr
@@ -162,10 +247,11 @@ if __name__ == '__main__':
         pr_aucs[fold] = pr_auc
     
         fold += 1
-        
-    # Get average performance across all k-folds
+    
+    # Get total performance from all PPI predictions (concatenated k-fold tested subsets)
     precision, recall, thresholds = metrics.precision_recall_curve(df_pred_avg[1], df_pred_avg[0])
-    precision = recalculate_precision(df_pred_avg, precision, thresholds)
+    if args.delta != 0.5:
+        precision = recalculate_precision(df_pred_avg, precision, thresholds, args.delta)
     fpr, tpr, __ = metrics.roc_curve(df_pred_avg[1], df_pred_avg[0])
     if args.delta == 0.5:
         pr_auc = metrics.average_precision_score(df_pred_avg[1], df_pred_avg[0])
@@ -173,31 +259,68 @@ if __name__ == '__main__':
         pr_auc = metrics.auc(recall, precision)
     roc_auc = metrics.roc_auc_score(df_pred_avg[1], df_pred_avg[0])
     
+    if args.delta <= 0.5:
+        leg_loc = 'lower right'
+    else:
+        leg_loc = 'upper right'
+    
+    # Get other metrics at 0.5 threshold if predictions are probabilities (0 to 1) i.e. not SPRINT predictions
+    if df_pred_avg[0].min() >= 0 and df_pred_avg[0].max() <= 1:
+        evaluation = ('accuracy=%.5f (+/- %.5f)'%(np.mean(avg_accuracy), np.std(avg_accuracy))
+                      + '\nprecision=%.5f (+/- %.5f)'%(np.mean(avg_precision), np.std(avg_precision)) 
+                      + '\nrecall=%.5f (+/- %.5f)'%(np.mean(avg_recall), np.std(avg_recall)) 
+                      + '\nspecificity=%.5f (+/- %.5f)'%(np.mean(avg_specificity), np.std(avg_specificity)) 
+                      + '\nf1=%.5f (+/- %.5f)'%(np.mean(avg_f1), np.std(avg_f1)) 
+                      + '\nmcc=%.5f (+/- %.5f)'%(np.mean(avg_mcc), np.std(avg_mcc))
+                      + '\nroc_auc=%.5f (+/- %.5f)' % (np.mean(np.fromiter(roc_aucs.values(), dtype=float)), np.std(np.fromiter(roc_aucs.values(), dtype=float)))
+                      + '\npr_auc=%.5f (+/- %.5f)' % (np.mean(np.fromiter(pr_aucs.values(), dtype=float)), np.std(np.fromiter(pr_aucs.values(), dtype=float)))
+                      + '\nroc_auc_overall=%.5f' % (roc_auc)
+                      + '\npr_auc_overall=%.5f' % (pr_auc)
+                      + '\n')
+    else:
+        evaluation = ('\nroc_auc=%.5f (+/- %.5f)' % (np.mean(np.fromiter(roc_aucs.values(), dtype=float)), np.std(np.fromiter(roc_aucs.values(), dtype=float)))
+                      + '\npr_auc=%.5f (+/- %.5f)' % (np.mean(np.fromiter(pr_aucs.values(), dtype=float)), np.std(np.fromiter(pr_aucs.values(), dtype=float)))
+                      + '\nroc_auc_overall=%.5f' % (roc_auc)
+                      + '\npr_auc_overall=%.5f' % (pr_auc)
+                      + '\n')        
+
+    print('\n===== EVALUATION =====')
+    print(evaluation)
+    print('Writing metrics to file...')
+    with open(RESULTS_DIR + 'evaluation_' + args.name + '.txt', 'w') as fp:
+        fp.write(evaluation)
+        
     # Plot and save curves
+    print("Plotting precision-recall")
     # Precision-Recall
     plt.figure
+    print("\t...average curve...")
     plt.plot(recall, precision, color='black', label='AUC = %0.4f +/- %0.4f' % (pr_auc, np.std(np.fromiter(pr_aucs.values(), dtype=float))))
     for i in recalls.keys():
+        print("\t...fold-%s curve..."%i)
         plt.plot(recalls[i], precisions[i], alpha=0.25)
     plt.xlabel('Recall')
     plt.ylabel('Precision') 
     plt.xlim([-0.05, 1.05])
     plt.ylim([-0.05, 1.05])
-    plt.title("Precision-Recall Curve - %s %s"%(args.name.capitalize(), RATIO))
-    plt.legend(loc='lower right', handlelength=0)
+    plt.title("Precision-Recall Curve - %s %s"%(args.name, RATIO))
+    plt.legend(loc=leg_loc, handlelength=0)
     plt.savefig(RESULTS_DIR + args.name + '_PR.png', format='png')
     plt.close()
     
     # ROC
+    print("Plotting ROC")
     plt.figure
+    print("\t...average curve...")
     plt.plot(fpr, tpr, color='black', label='AUC = %0.4f +/- %0.4f' % (roc_auc, np.std(np.fromiter(roc_aucs.values(), dtype=float))))
     for i in fprs.keys():
+        print("\t...fold-%s curve..."%i)
         plt.plot(fprs[i], tprs[i], alpha=0.25)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.xlim([-0.05, 1.05])
     plt.ylim([-0.05, 1.05])
-    plt.title("ROC Curve - %s %s"%(args.name.capitalize(), RATIO))
-    plt.legend(loc='lower right', handlelength=0)
+    plt.title("ROC Curve - %s %s"%(args.name, RATIO))
+    plt.legend(loc=leg_loc, handlelength=0)
     plt.savefig(RESULTS_DIR + args.name + '_ROC.png', format='png')
     plt.close()
